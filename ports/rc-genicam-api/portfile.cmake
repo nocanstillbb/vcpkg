868ed1153b0f7409fa318b9fc4b17f35d471ee36
hbb <huangbinbin@deepvision-tech.com>
2023-11-14 13:56:14 +0800

    [rc-genicam-api] update versions

--- ports/rc-genicam-api/portfile.cmake
+++ ports/rc-genicam-api/portfile.cmake
@@ -1,9 +1,10 @@
 vcpkg_from_git(
   OUT_SOURCE_PATH SOURCE_PATH
-  REPO http://git1.deepvision-tech.net/PACKAGES/rc_genicam_api
-    REF c453baf83a7237b011347facb731b9354697a321
+  URL http://git1.deepvision-tech.net/PACKAGES/rc_genicam_api.git
+  REF d553966c3dd0c92460392dca8282f13ee3fa4aa8
   HEAD_REF master
 )
+
 
 
 #https://learn.microsoft.com/en-us/vcpkg/examples/packaging-github-repos
@@ -14,6 +15,10 @@ vcpkg_cmake_install()
 vcpkg_fixup_pkgconfig()
 vcpkg_copy_pdbs()
 
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
 file(
     INSTALL "${SOURCE_PATH}/usage"
     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+
